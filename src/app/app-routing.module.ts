27f129aa20c0401fe170d3a0abcb67197f6cd6cf
yasmine-mahjoubi <yassminemahjoubi764@gmail.com>
2024-06-08 19:02:41 +0200

    Sprint1: création d'un composant profil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'profil', component :ProfilComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
