9bb05723e12164d51e2f450a3a62ae304b27db06
nourHasnawi <hasnaouinour931@gmail.com>
2024-06-10 00:11:00 +0200

    nchlh takhtef

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
 
 @NgModule({
   declarations: [
