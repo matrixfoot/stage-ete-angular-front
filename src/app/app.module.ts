31b7b742ed1f9514e9dbb69517c7a7aa3de108d8
matrixfoot <kammounomarindus@gmail.com>
2024-06-11 15:03:59 +0100

    problems on calendar compo fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
 import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarComponent } from './calendar/calendar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
