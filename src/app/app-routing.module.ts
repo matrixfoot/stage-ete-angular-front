add691a516bb7f6d71e92dc3dccede0aad4215aa
yasmine-mahjoubi <yassminemahjoubi764@gmail.com>
2024-06-15 01:02:07 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
 import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AboutComponent } from './about/about.component';
+import { AjouterEventComponent } from './ajouter-event/ajouter-event.component';
+import { PourquoiComponent } from './pourquoi/pourquoi.component';
+import { ModiEventComponent } from './modi-event/modi-event.component';
+import { CareerComponent } from './career/career.component';
+
 
 const routes: Routes = [
   {path:'calendar',component:CalendarComponent},
-  {path:'home',component:HomeComponent},
-
+  {path:'home',component:HomeComponent},{ path: 'profil', component :ProfilComponent},
+  { path: 'about', component :AboutComponent},{ path: 'ajouter-event', component :AjouterEventComponent},{ path: 'pourquoi', component :PourquoiComponent},{ path: 'modi-event', component :ModiEventComponent},
+  { path: 'career', component :CareerComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarComponent } from './calendar/calendar.component';
 import { HomeComponent } from './home/home.component';
-
+import { ProfilComponent } from './profil/profil.component';
+import { AboutComponent } from './about/about.component';
+import { AjouterEventComponent } from './ajouter-event/ajouter-event.component';
+import { PourquoiComponent } from './pourquoi/pourquoi.component';
+import { ModiEventComponent } from './modi-event/modi-event.component';
+import { CareerComponent } from './career/career.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    HomeComponent
+    HomeComponent,
+    ProfilComponent,
+    AboutComponent,
+    AjouterEventComponent,
+    PourquoiComponent,
+    ModiEventComponent,
+    CareerComponent
   ],
   imports: [
     ReactiveFormsModule,
