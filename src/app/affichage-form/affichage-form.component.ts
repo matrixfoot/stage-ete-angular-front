7441d7e8756b03ffcc200c2eb555d5bf7dda1d69
omar <omarabdelkader58@gmail.com>
2024-06-17 18:33:10 +0200

    update

--- src/app/affichage-form/affichage-form.component.ts
+++ src/app/affichage-form/affichage-form.component.ts
@@ -21,6 +21,40 @@ export class AffichageFormComponent {
   goBack() {
     this.location.back();
   }
+   addFields() {
+    const container = document.createElement('div');
+    container.classList.add('row');
+
+    const compteTTC = `
+      <div class="col-md-4 mt-4">
+        <label for="validationDefault02" class="form-label">Compte TTC</label>
+        <input type="text" class="form-control" id="validationDefault02" value="" required />
+      </div>
+    `;
+    const compteTVA = `
+      <div class="col-md-4 mt-4">
+        <label for="validationDefault02" class="form-label">Compte TVA</label>
+        <input type="text" class="form-control" id="validationDefault02" value="" required />
+      </div>
+    `;
+    
+    const tauxTVA = `
+      <div class="col-md-4 mt-4">
+        <label for="validationDefault04" class="form-label">Taux TVA</label>
+        <select class="form-select" id="validationDefault04" required>
+          <option>7%</option>
+          <option>13%</option>
+          <option>19%</option>
+          <option>exonere</option>
+          <option>export</option>
+          <option>suspension</option>
+        </select>
+      </div>
+    `;
+
+    container.innerHTML = compteTTC + tauxTVA+compteTVA;
+    document.getElementById('dynamic-fields')?.appendChild(container);
+  }
 
 }
 
