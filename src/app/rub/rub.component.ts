1057cc3d2bffded69c78729a484583dd66fb2df1
nourHasnawi <hasnaouinour931@gmail.com>
2024-06-25 22:02:33 +0200

    dec-fiscale-saisi

--- src/app/rub/rub.component.ts
+++ src/app/rub/rub.component.ts
@@ -1,26 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rub',
   templateUrl: './rub.component.html',
   styleUrls: ['./rub.component.scss']
 })
-export class RubComponent  implements OnInit {
-  @Input() rubriqueNumber: number;
+export class RubComponent {
+  showSoumistva: boolean = false; // Contrôle l'affichage du composant soumistva
+  showSoumistvaa: boolean = false; // Contrôle l'affichage du composant soumistva
+  rubriqueNumber: number;
+  static rubriqueCount: number =2; // Commence à partir de 1 pour le premier élément
   showComponent: boolean = true;
+  
+  constructor() {
+    this.rubriqueNumber = RubComponent.rubriqueCount;
+    RubComponent.rubriqueCount++;
+  }
 
-  constructor() {}
+  showSoumistvaComponent() {
+    this.showSoumistva = true;
+  }
 
+  deleteRubrique() {
+    this.showSoumistvaa = false; // Masque le composant lorsque supprimé
+  }
   ngOnInit(): void {
     // Initialize component
   }
 
   deleteComponent() {
     this.showComponent = false;
-  }
-
-  showSoumistvaComponent() {
-    // Votre logique pour montrer le composant
   }
 }
